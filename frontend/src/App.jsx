ce9ba3e1c007dd21f4bb103ea7ec5008a7157e25
Dereck Cabrera <dereckcabrerang@gmail.com>
2024-05-30 12:51:40 -0600

    Added styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,74 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import "./App.css";
+
+const Row = ({ dpi, first_name }) => {
+  return (
+    <div>
+      {dpi}
+      {first_name}
+    </div>
+  );
+};
+
+function Modal({ openModal, closeModal, children }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (openModal) {
+      ref.current?.showModal();
+    } else {
+      ref.current?.close();
+    }
+  }, [openModal]);
+
+  return (
+    <dialog ref={ref} onCancel={closeModal}>
+      <div>
+        {children}
+        <button onClick={closeModal} className="btn" type="button">
+          Cerrar
+        </button>
+      </div>
+    </dialog>
+  );
+}
+
+const Button = ({ text, onClick }) => {
+  return (
+    <button className="btn" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
 function App() {
+  const [database, setDatabase] = useState(null);
   const [file, setFile] = useState(null);
-  const [message, setMessage] = useState("");
+  const [result, setResult] = useState([]);
+  const [modal, setModal] = useState(false);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
 
+  const handleDatabaseChange = (event) => {
+    setDatabase(event.target.files[0]);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!file) {
-      alert("Please select a file first!");
+      alert("Please select the auction!");
+      return;
+    }
+    if (!database) {
+      alert("Please select the customers database!");
       return;
     }
 
     const formData = new FormData();
     formData.append("file", file);
+    formData.append("database", database);
     try {
       const response = await fetch("http://localhost:8000/upload", {
         method: "POST",
@@ -24,26 +76,76 @@ function App() {
       });
 
       if (response.ok) {
-        const data = await response.json();
+        let data = await response.json();
+        data = data.map((current) => JSON.parse(current));
         alert("File uploaded successfully!");
-        setMessage(`${JSON.stringify(data)}`);
+        setResult(data);
       } else {
         alert("File upload failed!");
-        setMessage("");
+        setResult(null);
       }
     } catch (error) {
       alert(`Error uploading file: ${error.message}`);
     }
   };
+
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <input type="file" onChange={handleFileChange} />
-          <button type="submit">Upload</button>
-        </form>
+        <h1>InMuebles GT: Interfaz de busqueda</h1>
+        <Button text="Salir" onClick={null} />
       </header>
-      <p>{message}</p>
+      <main className="content">
+        <div className="blocker">Ingrese datos</div>
+        {result.map((current) => (
+          <Row
+            key={current.dpi}
+            dpi={current.dpi}
+            first_name={current.first_name}
+          />
+        ))}
+      </main>
+      <div className="instructions">
+        <p className="instructions-title">Instrucciones</p>
+        <ul className="instructions-list">
+          <li>
+            Ingresar los Json´s con el botón inferior, ingresar clientes y
+            postores.
+          </li>
+          <li>Los clientes ganadores se mostraran en la pantalla</li>
+          <li>Ingresar solamente Json´s validos</li>
+        </ul>
+      </div>
+      <button onClick={() => setModal(true)} className="btn" type="button">
+        Ingresar Datos
+      </button>
+      <div>
+        <Modal openModal={modal} closeModal={() => setModal(false)}>
+          <form onSubmit={handleSubmit} className="main-form">
+            <div className="input-container">
+              <label htmlFor="databse">Clientes</label>
+              <input
+                type="file"
+                onChange={handleDatabaseChange}
+                id="database"
+                name="database"
+              />
+              <label htmlFor="file">Subasta</label>
+              <input
+                type="file"
+                onChange={handleFileChange}
+                id="file"
+                name="file"
+              />
+            </div>
+            <div className="upload">
+              <button type="submit" className="btn">
+                Upload
+              </button>
+            </div>
+          </form>
+        </Modal>
+      </div>
     </div>
   );
 }
